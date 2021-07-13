be05f482a620a7f2db4525128163458372eb85d0
hannahchun <80413027+hannahchun@users.noreply.github.com>
2021-07-13 14:45:15 +0900

    Add files via upload

---
+++ 혜선/6-2-sol1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+	queue <int> a;
+	int n, k, i ;
+	cin >> n ;
+	cin >> k ;
+
+	//1~n의 숫자를 차례로 큐에 넣어줌 
+	for (int i=1 ; i<=n ;i++)
+	 	a.push(i);
+	cout<<"<";
+	i=1;
+	while(n-1){
+		// i 나누기 k의 나머지가 0이 아닐 때 front 값을 다시 큐에 넣고 기조노 값은 pop
+		if ( i % k !=0){
+			a.push(a.front());
+			a.pop();
+		}
+		// i 나누기 k의 나머지가 0일 때 front 값을 출력하고 pop
+		else{
+			cout << a.front() << ", ";
+			a.pop();
+			n--;
+		}
+		i++;
+	}
+	cout<<a.front()<<">";
+	return 0;
+}
