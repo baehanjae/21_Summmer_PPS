ce848c9eb8aec147585e666a5f9bb989e8f9eba2
hannahchun <80413027+hannahchun@users.noreply.github.com>
2021-07-13 14:49:35 +0900

    Update 6-2-sol2.cpp

--- 혜선/6-2-sol2.cpp
+++ 혜선/6-2-sol2.cpp
@@ -15,12 +15,16 @@ int main(){
         a.push(i);
     cout<<"<";
     
+    //큐가 비어있을 때까지 계속 실행 
     while(!a.empty()){
+        
+        //k의 배수 번째에 다다르기 전까는 큐의 front를 꺼내서 다시 큐의 마지막에 삽입
         for ( int i=0 ; i<k-1 ; i++){
             a.push(a.front());
             a.pop();
         }
-
+        
+        //k의 배수 번째는 출력 후 삭제 
         if(1<a.size())
             cout<<a.front()<<", ";
         else
