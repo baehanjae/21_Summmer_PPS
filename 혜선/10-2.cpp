41409ee1d10a84f99fb2fd8cb9a8746d8e20b890
hannahchun <80413027+hannahchun@users.noreply.github.com>
2021-07-21 23:34:23 +0900

    Update 10-2.cpp

--- 혜선/10-2.cpp
+++ 혜선/10-2.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-
-using namespace std;
-
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
     public:
         TreeNode* increasingBST(TreeNode* root) {
-            TreeNode dummy(0);
-            prev=&dummy; //dummy node로 초기화 
+            TreeNode *dummy = new TreeNode(0);
+            prev=dummy; //dummy node로 초기화 
             inorder(root);
-            return dummy.right;
+            return dummy->right;
         }
 
     //left node -> root -> right node 순서로 node를 뽑아내어 구성해야 (inorder traverse를 해야!)
@@ -36,7 +33,5 @@ class Solution {
         }    
 };
 
-//9-2부터 문제가 너무 어려워요ㅠㅠ
-//출처: https://zxi.mytechroad.com/blog/tree/leetcode-897-increasing-order-search-tree/ 
-//보면서 최대한 이해를 해보았는데, 완전히 깨달았는지는 잘 모르겠습니다
-//이번주에 더 공부하겠습니다 
+//출처: https://www.programmersought.com/article/73044934134/
+
